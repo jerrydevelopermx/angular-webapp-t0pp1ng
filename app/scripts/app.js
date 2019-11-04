a1b0ce0b16cfa13780e099809c11469abb188d3b
Gerardo Garcia <jerry@Gerardos-MacBook-Pro.local>
2019-11-03 23:03:54 -0600

    Added google analytics support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
       'nemLogging',
       'ngSanitize',
       'slickCarousel',
-      'vcRecaptcha'
+      'vcRecaptcha',
+      'angular-google-analytics'
   ])
 
 
@@ -24,6 +25,33 @@ angular
   'api_url' : 'http://toppingcreativo.com.mx/topping-api/index.php/'//'http://localhost/topping-api/index.php/'
 })
 
+
+.config(function (AnalyticsProvider) {
+        // initial configuration
+        AnalyticsProvider.setAccount('UA-151362070-1');
+        // track all routes/states (or not)
+        AnalyticsProvider.trackPages(true);
+        // Use analytics.js instead of ga.js
+        AnalyticsProvider.useAnalytics(true);
+        // change page event name
+        AnalyticsProvider.setPageEvent('$stateChangeSuccess');
+
+
+})
+
+.run(function($rootScope, $location, $window, $transitions, Analytics) {
+  $transitions.onSuccess({}, function($transition){
+        var url = $transition.$to().self.url;
+        var params = $transition.params();
+        if(params.post_id !== undefined){
+          var urlSplit = url.split("/");
+          urlSplit[urlSplit.length-1] = params.post_id;
+          url = urlSplit.join('/');
+        }
+        Analytics.trackPage(url);
+    });
+
+})
 /*
   .run(function($rootScope) {
     $rootScope.$on("$locationChangeStart", function(event, next, current) {
